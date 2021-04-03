2f655602bfa8cf07a9dfb0859a5df6f4bef02a33
Pedrofenti <pedro.fernandez@enti.cat>
2021-04-03 11:56:09 +0200

    Animacion de las Setas Saltimbanquis

---
+++ Assets/Scripts/MushroomController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MushroomController : MonoBehaviour
+{
+    private Rigidbody2D rigidBody;
+    private Animator animator;
+    private int bounce_animation;
+    private bool isBouncing = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        bounce_animation = Animator.StringToHash("isBouncing");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetBool(bounce_animation, isBouncing);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Spikey")
+        {
+            isBouncing = true;
+        }
+    }
+
+    private void ResetAnimation() { isBouncing = false; }
+}
