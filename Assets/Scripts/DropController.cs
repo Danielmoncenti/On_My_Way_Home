f6c40ea8cae20553a87ba689d193da5a00da4b97
Pedrofenti <pedro.fernandez@enti.cat>
2021-04-03 15:34:33 +0200

    Animaciones y correcciones de la Gota Toxica

--- Assets/Scripts/DropController.cs
+++ Assets/Scripts/DropController.cs
@@ -4,30 +4,48 @@ using UnityEngine;
 
 public class DropController : MonoBehaviour
 {
-    //animacion
-    //sonido
+    private Rigidbody2D rigidBody;
+    private Animator animator;
+    private int hold_animation;
+    private int splash_animation;
+    private bool isHolding = false;
+    private bool splash = false;
+    private bool stop = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        //animacion
-        //sonido
+        rigidBody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        hold_animation = Animator.StringToHash("isHolding");
+        splash_animation = Animator.StringToHash("Splash");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        if (stop)
+        {
+            rigidBody.velocity = Vector3.zero;
+            rigidBody.gravityScale = 0;
+        }
+
+        animator.SetBool(hold_animation, isHolding);
+        animator.SetBool(splash_animation, splash);
     }
-    private void OnCollisionEnter2D(Collision2D collision)
+
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Tilemap")
         {
+            stop = true;
+            splash = true;
             SoundManager.Play(SoundManager.Sound.DROP);
-            Destroy(gameObject);
-            //animacion
         }
-
-
     }
+
+    private void HoldAnimation() { isHolding = true; }
+
+    private void DestroyDrop() { Destroy(gameObject); }
 }
