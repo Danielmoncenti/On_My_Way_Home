d0da7292f3c1bf7b3c6dc029d335906575d45ecc
Pedrofenti <pedro.fernandez@enti.cat>
2021-04-03 11:17:11 +0200

    Animaciones y limpieza del proyecto

--- Assets/Scripts/BushController.cs
+++ Assets/Scripts/BushController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class bushcontroller : MonoBehaviour
+public class BushController : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
---
+++ Assets/Scripts/TrapController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapController : MonoBehaviour
+{
+    [SerializeField] GameObject Spikey;
+    private Rigidbody2D rigidBody;
+    private Animator animator;
+    private int itsAtrap_animation;
+    private int isWaiting_animation;
+    private bool isTrapped = false;
+    private bool isWaiting = false;
+    private float stuckedtimer = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        itsAtrap_animation = Animator.StringToHash("isTrapped");
+        isWaiting_animation = Animator.StringToHash("isWaiting");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetBool(itsAtrap_animation, isTrapped);
+        animator.SetBool(isWaiting_animation, isWaiting);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Spikey")
+        {
+            isTrapped = true;
+        }
+    }
+
+    private void FreezeAnimation() { isWaiting = true; }
+
+    private void ResetAnimation() 
+    {
+        isWaiting = false;
+        isTrapped = false;
+        /*stuckedtimer += Time.deltaTime * 1000;
+        if (stuckedtimer >= 2000)
+        {
+            isWaiting = false;
+            isTrapped = false;
+            stuckedtimer = 0;
+        }*/
+    }
+}
