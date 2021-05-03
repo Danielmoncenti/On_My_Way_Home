14e948a8627e4f87c23ab39f7b06531f8c90b49d
jtomasenti <jordi.tomas@enti.cat>
2021-05-03 12:09:28 +0200

    push para el daniel pesao

--- Assets/Scripts/RockController.cs
+++ Assets/Scripts/RockController.cs
@@ -8,7 +8,7 @@ public class RockController : MonoBehaviour
     private GameObject Spikey;
 
     private float angle = 45.0f;
-    private float thurst = 0.6f;
+    private float thurst = 0.55f;
     private Vector3 speed;
     private Vector3 distance;
     private Vector3 targetposition;
@@ -26,16 +26,27 @@ public class RockController : MonoBehaviour
         _rb2d = GetComponent<Rigidbody2D>();
 
         distance = Rock.transform.position - Spikey.transform.position;
+        if (distance.x > 0) {
+            direction = new Vector3(Mathf.Sin(angle * Mathf.Deg2Rad) * -1, Mathf.Cos(angle * Mathf.Deg2Rad), 0);
 
-        direction = new Vector3(Mathf.Sin(angle * Mathf.Deg2Rad) * -1, Mathf.Cos(angle * Mathf.Deg2Rad), 0);
+            direction.Normalize();
 
-        direction.Normalize();
+            thurst = thurst * distance.x;
 
-        thurst = thurst * distance.x;
+            speed = direction * thurst;
 
-        speed = direction * thurst;
+            _rb2d.AddForce(speed, ForceMode2D.Impulse);
+        }else {
+            direction = new Vector3(Mathf.Sin(angle * Mathf.Deg2Rad) * -1, Mathf.Cos(angle * Mathf.Deg2Rad), 0);
 
-        _rb2d.AddForce(speed, ForceMode2D.Impulse);
+            direction.Normalize();
+
+            thurst = thurst * distance.x;
+
+            speed = direction * thurst;
+            speed.y = speed.y * -1; 
+            _rb2d.AddForce(speed, ForceMode2D.Impulse);
+        }
     }
 
     // Update is called once per frame
